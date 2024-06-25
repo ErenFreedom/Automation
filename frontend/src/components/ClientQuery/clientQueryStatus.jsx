9e0acb696de18cb3e995d531554feb2528e03702
kartik <kartik@kali.kartik>
2024-06-25 10:36:44 +0100

    Added new backend and frontend files

--- frontend/src/components/ClientQuery/clientQueryStatus.jsx
+++ frontend/src/components/ClientQuery/clientQueryStatus.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import './ClientQueryStatus.css'; // Create this file for styling
+import './clientQueryStatus.css'; // Create this file for styling
 
 const ClientQueryStatus = () => {
   const { userId } = useParams();
