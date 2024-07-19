e95422c715f63981ca7aa497ec500a2dad701e18
kartik <kartik@kali.kartik>
2024-07-19 11:55:25 +0100

    Add updated code

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const authenticateToken = require('../middlewares/authenticateToken');
 const reportController = require('../controllers/reportController');
 
 // Route to generate report
-router.post('/generate-report', authenticateToken, reportController.generateReport);
+router.post('/generate-report', reportController.generateReport);
 
 module.exports = router;
