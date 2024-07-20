beb04109715623613d4ebd0be8ce189088e39842
kartik <kartik@kali.kartik>
2024-07-20 22:34:16 +0100

    Add updated code

--- routes/clientNotificationRoutes.js
+++ routes/clientNotificationRoutes.js
@@ -5,14 +5,14 @@ const clientNotificationController = require('../controllers/clientNotificationC
 const clientThresholdController = require('../controllers/clientThresholdController');
 
 // Route to set multiple thresholds
-router.post('/set-thresholds', authenticateToken, clientThresholdController.setThresholds);
+router.post('/client-set-thresholds', authenticateToken, clientThresholdController.setThresholds);
 
 // Route to get notifications
-router.get('/get-notifications', authenticateToken, clientNotificationController.getNotifications);
+router.get('/client-notifications/get-notifications', authenticateToken, clientNotificationController.getNotifications);
 
 // Route to get sensor APIs
-router.get('/sensor-apis', authenticateToken, clientThresholdController.getSensorApis);
+router.get('/client-sensor-apis', authenticateToken, clientThresholdController.getSensorApis);
 
-router.get('/current-thresholds', authenticateToken, clientThresholdController.getCurrentThresholds);
+router.get('/client-current-thresholds', authenticateToken, clientThresholdController.getCurrentThresholds);
 
 module.exports = router;
