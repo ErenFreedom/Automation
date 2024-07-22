1b5b42f3ea231b1924bfff0f9205396b7b73c2e2
kartik <kartik@kali.kartik>
2024-07-22 13:02:12 +0100

    Add updated code

--- frontend/src/components/notifications/Notifications.jsx
+++ frontend/src/components/notifications/Notifications.jsx
@@ -1,4 +1,3 @@
-// src/components/notifications/Notifications.jsx
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
