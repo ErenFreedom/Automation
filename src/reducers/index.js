32c4803e7fd5aae4d535b12cb5a4479c4181577d
kartik <kartik@kali.kartik>
2024-07-25 10:53:20 +0100

    Add updated code

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import dataReducer from './dataReducer';
-
-const rootReducer = combineReducers({
-  data: dataReducer,
-});
-
-export default rootReducer;
