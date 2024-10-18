c7ff48e65dc7de67db2de3010416d85d8cb1d199
kartik <kartik@kali.kartik>
2024-10-19 02:44:19 +0530

    Initial commit with cleaned project files

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: 'ec2-13-126-117-233.ap-south-1.compute.amazonaws.com',
+  origin: 'https://ec2-13-126-117-233.ap-south-1.compute.amazonaws.com',
+
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
