b17aec9e4ed4bc303c97dbf394b5afc46021822f
kartik <kartik@kali.kartik>
2024-10-21 18:04:58 +0530

    Added backend files

--- routes/cloudConnectRoutes.js
+++ routes/cloudConnectRoutes.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
-const { cloudLogin, connectToDatabase } = require('../controllers/connectToDatabase');
+const { connectToDatabase } = require('../controllers/connectToDatabase');
 const router = express.Router();
-
-// Route for cloud login
-router.post('/cloud-login', [
-    check('identifier', 'Identifier (email/username) is required').not().isEmpty(),
-    check('password', 'Password is required').not().isEmpty(),
-], cloudLogin);
 
 // Route for connecting to RDS via activation key and inserting data
 router.post(
